4f36130535c660f4b0caf79a9380e1c1e5d4e25f
Ioan Anghel <ioan.anghel123@gmail.com>
2023-12-01 00:02:13 +0200

    Modified the structure and implementation of the string table

--- string_table.h
+++ string_table.h
@@ -1,27 +1,51 @@
+#include <iostream>
+
 struct Node {
     std :: string info;
-    struct Node *next;
+    Node *next;
 };
 
 class StringTable {
-    struct Node *head;
+    Node *head;
 
 public:
-    StringTable() = default;
-    ~StringTable() = default;
+    StringTable() {
+        this->head = new Node;
+        std :: cout << "Creating StringTable" << std :: endl;
+        this->head->info = "";
+        std :: cout << "Head created" << std :: endl;
+        this->head->next = nullptr;
+        std :: cout << "StringTable created" << std :: endl;
+    }
+    ~StringTable() {
+        std :: cout << "StringTable destroyed" << std :: endl;
+        Node *currentNode = head;
+        while(currentNode != nullptr) {
+            Node *temp = currentNode;
+            currentNode = currentNode->next;
+            delete temp;
+        }
+    }
 
     int addString(std :: string _info) {
+        std :: cout << "Adding string: " << _info << std :: endl;
         Node *newNode = new Node;
         newNode->info = _info;
 
-        if(head == nullptr) {
-            head = newNode;
+        std :: cout << "Node created " << newNode->info << std :: endl;
+        std :: cout << "Head: " << this->head->info << std :: endl;
+        std :: cout << (this->head->next == nullptr) << std :: endl;
+
+        if(this->head->info == "") {
+            std :: cout << "Head is null" << std :: endl;
+            this->head = newNode;
             return 0;
         }
         else {
-            Node *currentNode = head;
+            Node *currentNode = this->head;
+            std :: cout << "Current Node: " << currentNode->info << std :: endl;
             int index = 0;
-            while(currentNode != nullptr) {
+            while(currentNode->next != nullptr) {
                 if(currentNode->info == _info) {
                     return index;
                 }
@@ -29,6 +53,7 @@ public:
                 index++;
             }
             currentNode->next = newNode;
+            index++;
             return index;
         }
     }
